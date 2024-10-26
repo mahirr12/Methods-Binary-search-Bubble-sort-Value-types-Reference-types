e4ac82b9ba0179f3a0edc116723b9004d80e1b56
Mahir <mahir.cumaliyev12@gmail.com>
2024-10-26 12:45:01 +0400

    Tasks are done

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SumEvensInt(7);
+            SumEvensVoid(5);
+            string name = "  Code  Acacemy   ";
+            name = StringEdit(name);
+            Console.WriteLine(name);
+            
+        }
+
+        #region task1
+
+        static int SumEvensInt(int num)
+        {
+            int sum = 0;
+            for (int i = 0; i <= num; i+=2)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+            return sum;
+        }
+        static void SumEvensVoid(int num)
+        {
+            int sum = 0;
+            for (int i = 0; i <= num; i += 2)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+        #endregion
+
+        #region task2
+
+        static string StringEdit(string name)
+        {
+            string newName = "";
+            for (int i = 0; i < name.Length; i++)
+            {
+                
+                if (name[i] == ' ')
+                {
+                    continue;
+                }
+                newName += name[i];
+            }
+           return newName;
+        }
+
+        #endregion
+    }
+}
